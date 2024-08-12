1b2d52070e6b5fbcbeba272c55f6ebb3074c8238
Manik Chadgal <manikchadgal5@gmail.com>
2024-08-12 16:23:01 +0530

    made function to take input form user

--- javascript.js
+++ javascript.js
@@ -6,3 +6,13 @@ function getComputerChoice(num){
 
 let num = Math.random()*10;
 console.log(getComputerChoice(num));
+
+
+function humanChoice(n){
+    if(n<=3) return "rock";
+    else if(n>3 && n<=6) return "paper";
+    else return "scissors";
+}
+
+let n = parseInt(prompt("enter a number"));
+console.log(humanChoice(n));
